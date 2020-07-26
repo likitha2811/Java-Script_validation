fccfb939fcb24570b555d2c41cc97e9aa7afd929
likitha2811 <60342674+likitha2811@users.noreply.github.com>
2020-07-26 10:10:11 -0700

    javascript validation

---
+++ registration_js_page.js
@@ -0,0 +1,158 @@
+function allLetter(uname)
+{ 
+var letters = /^[A-Za-z]+$/;
+if(uname.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('Username must have alphabet characters only');
+uname.focus();
+return false;
+}
+}
+function passid_validation(pass,mx,my)
+{
+var passid_len = passid.value.length;
+if (passid_len == 0 ||passid_len >= my || passid_len < mx)
+{
+alert("Password should not be empty / length be between "+mx+" to "+my);
+passid.focus();
+return false;
+}
+return true;
+}
+function alphanumeric(uadd)
+{ 
+var letters = /^[0-9a-zA-Z]+$/;
+if(uadd.value.match(letters))
+{
+return true;
+}
+else
+{
+alert('User address must have alphanumeric characters only');
+uadd.focus();
+return false;
+}
+}
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+}
+function isDate(inputText)
+  {
+  var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  // Match the date format through regular expression
+  if(inputText.value.match(dateformat))
+  {
+  document.form1.text1.focus();
+  //Test which seperator is used '/' or '-'
+  var opera1 = inputText.value.split('/');
+  var opera2 = inputText.value.split('-');
+  lopera1 = opera1.length;
+  lopera2 = opera2.length;
+  // Extract the string into month, date and year
+  if (lopera1>1)
+  {
+  var pdate = inputText.value.split('/');
+  }
+  else if (lopera2>1)
+  {
+  var pdate = inputText.value.split('-');
+  }
+  var dd = parseInt(pdate[0]);
+  var mm  = parseInt(pdate[1]);
+  var yy = parseInt(pdate[2]);
+  // Create list of days of a month [assume there is no leap year by default]
+  var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+  if (mm==1 || mm>2)
+  {
+  if (dd>ListofDays[mm-1])
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  }
+  if (mm==2)
+  {
+  var lyear = false;
+  if ( (!(yy % 4) && yy % 100) || !(yy % 400)) 
+  {
+  lyear = true;
+  }
+  if ((lyear==false) && (dd>=29))
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  if ((lyear==true) && (dd>29))
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  }
+  }
+  else
+  {
+  alert("Invalid date format!");
+  document.form1.text1.focus();
+  return false;
+  }
+  }
+function mob_val(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
+  if((inputtxt.value.match(phoneno))
+        {
+      return true;
+        }
+      else
+        {
+        alert("invalid number!enter correct mobile number");
+        return false;
+        }
+}
+
+function formValidate()
+{
+var uname = document.registration.uname;
+var pass = document.registration.pword;
+var mno = document.registration.mno;
+var email = document.registration.email;
+var gender = document.registration.gender;
+var dob = document.registration.dob;
+var addr = document.registration.addr;
+var lang = document.registration.lang; 
+if(allLeter(uname))
+{
+if(pass_validation(pass,7,10))
+{
+if(mob_val(mno))
+{
+if(ValidateEmail(email))
+{
+if(isDate(dob))
+{
+if(alphanumeric(addr))
+{ 
+
+}
+} 
+}
+} 
+}
+}
+return false;
+}
